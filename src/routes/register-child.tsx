7986bc08a93de02fd2226bb020e1927dff8b4a57
DaniV3611 <danivego2015@gmail.com>
2025-04-01 19:12:25 -0500

    Visual changes to parent view (Remove modal)

--- src/routes/register-child.tsx
+++ src/routes/register-child.tsx
@@ -1,8 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import RegisterChildForm from "../components/RegisterChildForm";
 import { useState } from "react";
-import Modal from "../components/Modal";
-import Button from "../components/Button";
 
 export const Route = createFileRoute("/register-child")({
   component: RouteComponent,
@@ -11,54 +9,38 @@ export const Route = createFileRoute("/register-child")({
 function RouteComponent() {
   const [qrCode, setQrCode] = useState("");
 
-  const [showModal, setShowModal] = useState(false);
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const data = Object.fromEntries(formData.entries());
-    console.log(data);
+    // console.log(data);
     const qrkey = JSON.stringify(data);
     const qrCode = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(
       qrkey
     )}&size=200x200`;
     // Register the child in the database
     setQrCode(qrCode);
-    setShowModal(true);
-    console.log(qrCode);
   };
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center gap-4">
       {!qrCode && <RegisterChildForm handleSubmit={handleSubmit} />}
-      {qrCode && !showModal && (
-        <div className="flex flex-col items-center justify-center gap-4">
+      {qrCode && (
+        <>
           <h1 className="text-xl font-bold text-center">Niño Registrado</h1>
           <p>Bienvenido al parque!! 🥳</p>
-          <Button
-            label="Ver Código QR"
-            onClick={() => {
-              setShowModal(true);
-            }}
-          />
+          <img src={qrCode} alt="No fue pósible cargar el codigo qr" />
+          <div className="p-4 bg-red-300/50 rounded-md text-red-700 text-center w-11/12 md:w-auto">
+            <span className="font-bold">IMPORTANTE!!</span> Toma screenshot de
+            tu código QR
+          </div>
           <button
-            className="px-4 py-2 rounded-md font-bold flex items-center justify-center bg-white shadow-md hover:bg-gray-100 active:bg-gray-300 duration-300"
+            className="px-4 py-2 cursor-pointer rounded-md font-bold flex items-center justify-center bg-white shadow-md hover:bg-gray-300 active:bg-gray-300 duration-300"
             onClick={() => setQrCode("")}
           >
             Registrar otro niño
           </button>
-        </div>
-      )}
-      {qrCode && showModal && (
-        <Modal onClose={() => setShowModal(false)}>
-          <h1 className="text-xl font-bold text-center">Niño Registrado</h1>
-          <p>Bienvenido al parque!! 🥳</p>
-          <img src={qrCode} alt="No fue pósible cargar el codigo qr" />
-          <div>
-            <span className="font-bold">IMPORTANTE!!</span> Toma screenshot de
-            tu código QR
-          </div>
-        </Modal>
+        </>
       )}
     </div>
   );
